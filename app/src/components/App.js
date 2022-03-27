70d832821b432d8011ac4a138a8159d2db2965ac
PURPLE77777 <stefanovich.pasha@yandex.by>
2022-03-27 20:07:08 +0300

    installed react-router-dom

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+import "../styles/app.css";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="app">
+            <Header></Header>
+            <Main></Main>
+            <Footer></Footer>
+        </div>
+    );
 }
 
 export default App;
